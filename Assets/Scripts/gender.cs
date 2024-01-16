efa91a8f09f20013ab2367bb5e5f64de9eaea6d8
Redsilog <91083594+Redsilog@users.noreply.github.com>
2024-01-16 23:42:47 +0800

    Character

---
+++ Assets/Scripts/gender.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gender : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
